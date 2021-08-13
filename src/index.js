5420e7dbaae81327b2778320a483d07ad82a18e0
PC-Rat <32428513+PC-Rat@users.noreply.github.com>
2021-08-13 20:33:50 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  module.exports = function check(str, bracketsConfig) {
+	// your solution
+	const stack = [];
+
+	if (str.length % 2 != 0) return false;
+
+	const openBrackets = ['(', '[', '{', '|'];
+	const bracketPairs = {
+		')': '(',
+		']': '[',
+		'}': '{',
+		'|': '|'
+	};
+	const bracketOk = (str) => {
+		for (i = 0; i < str.length; i++) {
+			let currentSymbol = str[i];
+			if (openBrackets.includes(currentSymbol)) {
+				stack.push(currentSymbol);
+			} else if (stack.length === 0)
+				return false;
+
+			let topSymbol = stack[stack.length - 1];
+
+			if (bracketPairs[currentSymbol] === topSymbol) {
+				stack.pop();
+			} else {
+				return false;
+			}
+		}
+	};
+
+	return stack.length === 0;
+}
 }
